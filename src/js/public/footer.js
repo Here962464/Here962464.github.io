094b05371779d94d5fa885ca284a8894d120ba46
xxx <zhaom@jufaanli.com>
2019-03-14 09:29:06 +0800

    footer

---
+++ src/js/public/footer.js
@@ -0,0 +1,29 @@
+var footer = new Vue({
+	el:"#footer",
+	data:{
+		heartColor: "#ff4444",
+		heartColor2:"blue"
+	},
+	methods:{
+
+	},
+	computed:{
+
+	},
+	created:function(){
+		// 生成爱心的随机颜色
+		var self = this;
+		var	color1,color2,color3;
+		setInterval(function(){
+			color1 = Math.floor((Math.random()*255)+1);
+			color2 = Math.floor((Math.random()*255)+1);
+			color3 = Math.floor((Math.random()*255)+1);
+			self.heartColor = 'rgb('+color1+','+color2+','+color3+')';
+			self.heartColor2 = 'rgb('+color3+','+color1+','+color2+')';
+		},1000);
+		
+		console.log("哈哈，既然你打开了控制台，我就给你讲个笑话吧：");
+		console.log("从前有座庙，庙里有个和尚，和尚在讲故事，故事是：");
+		console.log("从前有座庙，庙里有个和尚，和尚在讲故事，故事是...");
+	}
+});
---
+++ src/js/public/header.js
@@ -0,0 +1,53 @@
+var headerVm = new Vue({
+	el:"#header",
+	data: {
+		// 顶部导航
+	    menu:[{title:"首页"},{title:"项目经验"},{title:"关于我"},{title:"联系方式"}],
+	    arr:["index","experience","about","contact"]
+	},
+	methods:{
+		// 切换导航
+		tabMenu:function(index){
+			var arr = this.arr;
+			var url = this.getUrlParams('tabs');
+	     	for(var i=0;i<arr.length;i++){
+				if(index == i){
+					window.location.href = arr[i] +'.html?tabs=' + arr[i];
+				}
+			}
+	    },
+	    // 正则匹配url
+		getUrlParams: function(name) { 
+		    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); //定义正则表达式 
+		    var r = window.location.search.substr(1).match(reg);  
+		    if (r != null) return unescape(r[2]); 
+		  	return null; 
+		}
+	},
+	// 计算属性   加载默认样式
+	computed:{
+		// 顶部导航的active样式
+		iscur: function(e){
+			var arr = this.arr;
+			var url = this.getUrlParams('tabs');
+			for(var i=0;i<arr.length;i++){
+				if(url == arr[0]){
+					return 0;
+				}else if(url == arr[1]){
+					return 1;
+				}else if(url == arr[2]){
+					return 2;
+				}else if(url == arr[3]){
+					return 3;
+				}else{
+					return 0;
+				}
+			}
+			
+		}
+	},
+	// 生命周期，加载初始化样式
+	created:function(){
+		
+	}
+});
