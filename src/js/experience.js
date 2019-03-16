6d0ba2ccd30cec6c34195c5d4643cfaeab455283
xxx <zhaom@jufaanli.com>
2019-03-16 15:49:02 +0800

    bomb

--- src/js/experience.js
+++ src/js/experience.js
@@ -1,9 +1,48 @@
+var jufaApp = "";
 var content = new Vue({
 	el:"#content",
 	data:{
-		pic: "src/images/1.jpg"
+		pic: "src/images/1.jpg",
+		expr_line:[
+			{
+				title:"民营企业法制体检自测系统",
+				timeS:"2019-1",
+				timeE:"2019-1",
+				link:"https://legalcheck.jufaanli.com/",
+				side:"project_right",
+				isFirst:true,
+				isLast:false
+			},
+			{
+				title:"聚法案例APP",
+				timeS:"2018-12",
+				timeE:"2018-12",
+				link:"http://a.app.qq.com/o/simple.jsp?pkgname=com.jufaanli.wvapp",
+				side:"project_left",
+				isFirst:false,
+				isLast:false
+			},
+			{
+				title:"民营企业法制体检自测系统",
+				timeS:"2019-1",
+				timeE:"2019-1",
+				link:"https://legalcheck.jufaanli.com/",
+				side:"project_right",
+				isFirst:false,
+				isLast:true
+			}
+		]
+	},
+	methods:{
+		showPic: function(){
+			alert(1)
+		}
+	},
+	beforeCreate: function(){
+		
 	},
 	created: function(){
+		// 幻灯片
 		var index = 1;
 		var self = this;
 		setInterval(function(){
@@ -15,3 +54,4 @@ var content = new Vue({
 		},5000);
 	}
 });
+
--- src/js/public/header.js
+++ src/js/public/header.js
@@ -3,8 +3,7 @@ var headerVm = new Vue({
 	data: {
 		// 顶部导航
 	    menu:[{title:"首页"},{title:"项目经验"},{title:"关于我"},{title:"联系方式"}],
-	    arr:["index","experience","about","contact"],
-	    scroll:false
+	    arr:["index","experience","about","contact"]
 	},
 	methods:{
 		// 切换导航
@@ -49,16 +48,6 @@ var headerVm = new Vue({
 	},
 	// 生命周期，加载初始化样式
 	created:function(){
-		var self = this;
-		// 监听滚动
-		window.addEventListener('scroll',function(e){
-			var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
-		    // console.log(scrollTop)
-		    if(scrollTop > 60){
-		    	self.scroll = true;
-		    }else{
-		    	self.scroll = false;
-		    }
-		},false);
+		
 	}
 });
